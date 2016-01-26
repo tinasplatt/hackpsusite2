50be874cc66a4a07c271294fbf9fb80b08ad4698
Christina <christinajeanplatt@gmail.com>
2016-01-26 17:46:53 -0500

    fixed type

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $( "#header" ).css( "height", window.innerHeight);
+    // $( "#header" ).css( "height", window.innerHeight);
 
     var isMobile = true;
 
